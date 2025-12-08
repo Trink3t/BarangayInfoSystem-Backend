794b21ec842a6b329d210729cb7ba20796c4f25e
Aldrin Aureus <aldrinaureus.1050@gmail.com>
2025-12-08 18:46:33 +0800

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import app from "./app";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3000;
+app.use(cors({
+    origin: "http://localhost:*",
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"]
+}))
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
