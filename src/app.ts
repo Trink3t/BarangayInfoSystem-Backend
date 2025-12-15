f2eadd4b17b88ebf304a0a34293588b5292dc471
Trink3t <aldrinaureus.1050@gmail.com>
2025-12-15 21:21:10 +0800

    cors update

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,19 @@ import express from "express";
 import morgan from "morgan";
 import routes from "./routes";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 // import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import "dotenv/config";
 import app from "./app";
-import cors from "cors";
 
 const PORT = process.env.PORT || 3000;
-app.use(cors({
-    origin: "http://localhost:*",
-    methods: ["GET", "POST", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization", "Accept"]
-}))
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
